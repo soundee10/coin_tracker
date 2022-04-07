eb23b5733384653068760d5c12bbfe12b9b56997
unknown <malice@korea.ac.kr>
2022-04-08 01:41:14 +0900

    #11 React Query Part 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import styled, { createGlobalStyle } from "styled-components";
 import Router from "./Router";
+import {ReactQueryDevtools} from "react-query/devtools";
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -72,6 +73,7 @@ function App() {
   <>
   <GlobalStyle />
   <Router />
+  <ReactQueryDevtools initialIsOpen={true}/>
   </>
   );
 }
