f80af05170c5107c49c68902319cc1f707b44eeb
unknown <malice@korea.ac.kr>
2022-04-08 00:43:37 +0900

    #10 React Query Part 1

---
+++ src/api.tsx
@@ -0,0 +1,13 @@
+export async function fetchCoins(){
+    const response = await fetch("https://api.coinpaprika.com/v1/coins");
+    const json = await response.json();
+    return json;
+}
+
+// or 
+
+/*
+export function fetchCoins(){
+    return fetch("https://api.coinpaprika.com/v1/coins").then(response=>response.json);
+}
+*/
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoins } from "../api";
 
 const Container = styled.div`
  padding: 0px 20px;
@@ -65,23 +67,15 @@ interface CoinInterface{
 
 
 function Coins(){
-    const [coins,setCoins] = useState<CoinInterface[]>([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(()=>{
-        (async()=>{
-            const response = await fetch("https://api.coinpaprika.com/v1/coins");
-            const json = await response.json();
-            setCoins(json.slice(0,50));
-            setLoading(false);
-        })();
-    },[]);
+    const { isLoading, data } = useQuery<CoinInterface[]>("allCoins", fetchCoins)
+    
     return (
         <Container>
             <Header>
                 <Title> Coin </Title>
             </Header>
-            {loading ? (<Loader>"Loading..."</Loader>) : <CoinsList>
-                {coins.map((coin) => (
+            {isLoading ? (<Loader>"Loading..."</Loader>) : <CoinsList>
+                {data?.slice(0,50).map((coin) => (
                     <Coin key={coin.id}>
                     <Link to={{
                         pathname:`/${coin.id}`,
