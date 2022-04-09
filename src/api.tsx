427d20cade05f0d070a9444cbfcb7e604d78804b
unknown <malice@korea.ac.kr>
2022-04-09 22:41:29 +0900

    #13 Price Chart applied

--- src/api.tsx
+++ src/api.tsx
@@ -21,7 +21,7 @@ export async function fetchCoinTickers(coinId:string){
 
 export async function fetchCoinHistory(coinId:string){
     const endDate = Math.floor(Date.now() /1000);
-    const startDate = endDate - 60 * 60 * 24 * 7;
+    const startDate = endDate - 60 * 60 * 24 * 7 * 2;
     const response = await fetch(`${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`);
     const json = await response.json();
     return json;
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,6 +1,19 @@
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts"
+
+interface Ihistorical{
+
+    time_open: string;
+    time_close: string;
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    volume: number;
+    market_cap: number;
+}
 
 interface ChartProps{
     coinId: string,
@@ -8,9 +21,61 @@ interface ChartProps{
 
 function Chart({coinId}: ChartProps){
     //const params = useParams();
-    const {isLoading, data} = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId));
+    const {isLoading, data} = useQuery<Ihistorical[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId));
 
-    return <h1>Chart</h1>
+    return <div>{isLoading ? "Loading chart..." : 
+    <ApexChart
+        type="line"
+        series={[
+            {
+            name: "price",
+            data: data?.map(price=>price.close) as number[]
+            },
+        ]}
+        options={{
+            theme:{
+                mode: "dark"
+            },
+            chart:{
+                toolbar:{
+                    show:false
+                },
+                height: 500,
+                width: 500,
+                background: "transparent"
+            },
+            grid: {show:false},
+            stroke:{
+                curve: "smooth",
+                width: 3,
+            },
+            fill:{
+                type:"gradient",
+                gradient: {gradientToColors:["blue"]},
+                colors:[
+                    "red"
+                ],
+            },
+            tooltip:{
+                y:{
+                    formatter: (value) => `$ ${value.toFixed(2)}`
+                }
+            },
+            yaxis:{
+                tickAmount: 4,
+                labels:{
+                    formatter: (value) => `${value.toFixed(1)}`
+                }
+            },
+            xaxis:{
+                type:"datetime",
+                labels:{
+                    show: false,
+                },
+                categories: data?.map(price=>price.time_close)
+            }
+        }}
+    />}</div>;
 }
 
 export default Chart;
